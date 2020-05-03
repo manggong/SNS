5be2afefe1912f303ec8c81a36fa12d1169e05e0
manggong <jihwank1995@gmail.com>
2020-05-03 23:16:11 +0900

    Add middlewares

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,12 @@
 import * as express from "express";
+import * as cors from "cors";
+import * as morgan from "morgan";
+import * as cookieParser from "cookie-parser";
+import * as dotenv from "dotenv";
+import * as session from "express-session";
+import * as hpp from "hpp";
 
+dotenv.config();
 const app = express();
 
 const prod: boolean = process.env.NODE_ENV === "production";
